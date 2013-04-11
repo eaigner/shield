9816b720e8cca5dc3b629dfb8576b4e93b3a7bc7
Erik Aigner <erik@chocomoko.com>
2013-04-11 03:05:22 +0200

    Min 3 chars.

--- en.go
+++ en.go
@@ -14,9 +14,15 @@ func NewEnglishTokenizer() Tokenizer {
 func (t *enTokenizer) Tokenize(text string) (words map[string]int64) {
 	words = make(map[string]int64)
 	for _, w := range splitTokenRx.Split(text, -1) {
-		words[w]++
+		if len(w) > 2 {
+			words[w]++
+		}
 	}
 	return
 }
 
+// Spamassassin stoplist
+//
+// http://wiki.apache.org/spamassassin/BayesStopList
+//
 var splitTokenRx = regexp.MustCompile(`\s+|able|all|already|and|any|are|because|both|can|come|each|email|even|few|first|for|from|give|has|have|http|information|into|it's|just|know|like|long|look|made|mail|mailing|mailto|make|many|more|most|much|need|not|now|number|off|one|only|out|own|people|place|right|same|see|such|that|the|this|through|time|using|web|where|why|with|without|work|world|year|years|you|you're|your`)
