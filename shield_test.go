346d9c175823c8e1a68a28c6743a569b81da75eb
Erik Aigner <erik@chocomoko.com>
2013-04-11 03:17:14 +0200

    Legacy tests.

--- shield_test.go
+++ shield_test.go
@@ -45,6 +45,27 @@ func TestLearn(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// Legacy tests
+	sh.Learn("good", "sunshine drugs love sex lobster sloth")
+	sh.Learn("bad", "fear death horror government zombie god")
+
+	c, err := sh.Classify("sloths are so cute i love them")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c != "good" {
+		t.Fatal(c)
+	}
+
+	c, err = sh.Classify("i fear god and love the government")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c != "bad" {
+		t.Fatal(c)
+	}
+
+	// Run on test sets
 	for _, v := range trainData {
 		c := strings.SplitN(v, " ", 2)
 		sh.Learn(c[0], c[1])
